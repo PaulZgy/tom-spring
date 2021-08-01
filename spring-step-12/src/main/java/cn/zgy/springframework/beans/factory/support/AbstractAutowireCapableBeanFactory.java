00dbc66b620d1f83826da7dc44cde62622dac0cb
tom0949 <514199242@qq.com>
2021-08-01 12:08:35 +0800

    spring-step-12

--- spring-step-12/src/main/java/cn/zgy/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
+++ spring-step-12/src/main/java/cn/zgy/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
@@ -5,10 +5,7 @@ import cn.zgy.springframework.beans.BeansException;
 import cn.zgy.springframework.beans.PropertyValue;
 import cn.zgy.springframework.beans.PropertyValues;
 import cn.zgy.springframework.beans.factory.*;
-import cn.zgy.springframework.beans.factory.config.AutowireCapableBeanFactory;
-import cn.zgy.springframework.beans.factory.config.BeanDefinition;
-import cn.zgy.springframework.beans.factory.config.BeanPostProcessor;
-import cn.zgy.springframework.beans.factory.config.BeanReference;
+import cn.zgy.springframework.beans.factory.config.*;
 import org.apache.commons.beanutils.BeanUtils;
 
 import java.lang.reflect.Constructor;
@@ -22,6 +19,12 @@ public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFac
     protected Object createBean(String beanName, BeanDefinition beanDefinition, Object[] args) throws BeansException {
         Object bean = null;
         try {
+            //判断是否返回代理bean对象
+            bean = resolveBeforeInstantiation(beanName, beanDefinition);
+            if (null != bean) {
+                return bean;
+            }
+            //实例化bean
             bean = createBeanInstance(beanDefinition, beanName, args);
             //给bean填充属性
             applyPropertyValues(beanName, bean, beanDefinition);
@@ -38,6 +41,24 @@ public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFac
             registerSingleton(beanName,bean);
         }
         return bean;
+    }
+
+    protected Object resolveBeforeInstantiation(String beanName, BeanDefinition beanDefinition) {
+        Object bean = applyBeanPostProcessorsBeforeInstantiation(beanDefinition.getBeanClass(), beanName);
+        if (null != bean) {
+            bean = applyBeanPostProcessorsAfterInitialization(bean, beanName);
+        }
+        return bean;
+    }
+
+    protected Object applyBeanPostProcessorsBeforeInstantiation(Class<?> beanClass, String beanName) {
+        for (BeanPostProcessor beanPostProcessor : getBeanPostProcessors()) {
+            if (beanPostProcessor instanceof InstantiationAwareBeanPostProcessor) {
+                Object result = ((InstantiationAwareBeanPostProcessor) beanPostProcessor).postProcessBeforeInstantiation(beanClass, beanName);
+                if (null != result) return result;
+            }
+        }
+        return null;
     }
 
     protected void registerDisposableBeanIfNecessary(String beanName, Object bean, BeanDefinition beanDefinition) {
--- spring-step-12/src/test/java/springframework/ApiTest.java
+++ spring-step-12/src/test/java/springframework/ApiTest.java
@@ -5,6 +5,7 @@ import cn.zgy.springframework.aop.TargetSource;
 import cn.zgy.springframework.aop.aspectj.AspectJExpressionPointcut;
 import cn.zgy.springframework.aop.framework.Cglib2AopProxy;
 import cn.zgy.springframework.aop.framework.JdkDynamicAopProxy;
+import cn.zgy.springframework.context.support.ClassPathXmlApplicationContext;
 import cn.zgy.springframework.core.io.DefaultResourceLoader;
 import org.junit.Test;
 
@@ -19,34 +20,10 @@ public class ApiTest {
     private DefaultResourceLoader resourceLoader;
 
     @Test
-    public void test_aop() throws NoSuchMethodException {
-        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut("execution(* springframework.beans.UserService.*(..))");
-        Class<UserService> clazz = UserService.class;
-        Method method = clazz.getDeclaredMethod("queryUserInfo");
-
-        System.out.println(pointcut.matches(clazz));
-        System.out.println(pointcut.matches(method, clazz));
+    public void test_aop() {
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring.xml");
+        IUserService userService = applicationContext.getBean("userService", IUserService.class);
+        System.out.println("测试结果： " + userService.queryUserInfo());
     }
 
-    @Test
-    public void test_dynamic() {
-        //目标对象
-        IUserService userService = new UserService();
-
-        //组装代理信息
-        AdvisedSupport advisedSupport = new AdvisedSupport();
-        advisedSupport.setTargetSource(new TargetSource(userService));
-        advisedSupport.setMethodInterceptor(new UserServiceInterceptor());
-        advisedSupport.setMethodMatcher(new AspectJExpressionPointcut("execution(* springframework.beans.IUserService.*(..))"));
-
-        //代理对象(JdkDynamicAopProxy)
-        IUserService proxy_jdk = (IUserService) new JdkDynamicAopProxy(advisedSupport).getProxy();
-        //测试调用
-        System.out.println("测试结果：" + proxy_jdk.queryUserInfo());
-
-        // 代理对象(Cglib2AopProxy)
-        IUserService proxy_cglib = (IUserService) new Cglib2AopProxy(advisedSupport).getProxy();
-        // 测试调用
-        System.out.println("测试结果：" + proxy_cglib.register("彭于晏"));
-    }
 }
